3a6d1bcd148684e27a84b9fcbbeff718f79fc2f0
Kenneth Baldauf <kenneth.baldauf@originate.com>
2016-08-04 16:57:22 -0700

    Added basic LeakCanary implementation.

--- app/src/main/java/com/kbaldauf/playerfinder/PlayerFinderApplication.java
+++ app/src/main/java/com/kbaldauf/playerfinder/PlayerFinderApplication.java
@@ -9,6 +9,8 @@ import com.kbaldauf.playerfinder.component.DaggerApplicationComponent;
 import com.kbaldauf.playerfinder.component.DaggerNetworkComponent;
 import com.kbaldauf.playerfinder.component.NetworkComponent;
 import com.kbaldauf.playerfinder.module.ApplicationModule;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import timber.log.Timber;
 
@@ -16,6 +18,7 @@ public class PlayerFinderApplication extends Application {
 
     private ApplicationComponent applicationComponent;
     private NetworkComponent networkComponent;
+    private RefWatcher refWatcher;
 
     @Override
     public void onCreate() {
@@ -23,8 +26,13 @@ public class PlayerFinderApplication extends Application {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
+        refWatcher = LeakCanary.install(this);
         applicationComponent = DaggerApplicationComponent.builder().applicationModule(new ApplicationModule(this)).build();
         networkComponent = DaggerNetworkComponent.builder().applicationComponent(applicationComponent).build();
+    }
+
+    public RefWatcher getRefWatcher() {
+        return refWatcher;
     }
 
     public ApplicationComponent getApplicationComponent() {
